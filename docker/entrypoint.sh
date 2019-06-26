3b762405b4b0be51c75b05de87f182dda3fb4dd8
Colin Coleman <colin.coleman@kontrollsmart.com>
2019-06-26 13:03:25 +0200

    paramaterise EULA and JAVAPARAMS

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 if [ ! -f /home/minecraft/data/eula.txt ]
 then
-    echo "eula=true" > /home/minecraft/data/eula.txt
+    echo "eula=$EULA" > /home/minecraft/data/eula.txt
 fi
 cd /home/minecraft/data
-java -XX:+UseG1GC -Xmx4G -Xms4G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar /home/minecraft/server.jar nogui
+if [ -ne $JAVAPARAMS ]
+then
+    JAVAPARAMS='-Xmx1024M -Xms1024M'
+fi
+java $JAVAPARAMS -jar /home/minecraft/server.jar nogui
